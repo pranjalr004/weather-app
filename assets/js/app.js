bd23700bfb5545d9b015deb49b9374012aaf65ef
pranjalr004 <pranjalr376@gmail.com>
2025-03-05 21:00:37 +0530

    Search Toggle for Mobile Device Done

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,5 +1,25 @@
+'use strict';
+
+import { fetchData,url } from "./api.js";
+import * as module from "./module.js"
+
 /**
- * @license MIT
- * @copyright codewithsadee 2023 All rights reserved
- * @author codewithsadee <mohammadsadee24@gmail.com>
+ * 
+ * @param {NodeList} elements Elements node array
+ * @param {string} eventType Event Type ex-"click","mouseover"
+ * @param {Function} callback Callback function
  */
+
+const addEventOnElements=function(elements,eventType,callback){
+    for(const element of elements) element.addEventListener(eventType,callback);
+}
+
+/**
+ * Toggle Search in mobile devices
+ */
+
+const searchView=document.querySelector("[data-search-view]");
+const searchTogglers=document.querySelectorAll("[data-search-toggler]");
+
+const toggleSearch=()=>searchView.classList.toggle("active");
+addEventOnElements(searchTogglers,"click",toggleSearch)
