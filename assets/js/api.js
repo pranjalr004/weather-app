1cdf1c73c891004fe974e2778d1a0c6919589ac7
pranjalr004 <pranjalr376@gmail.com>
2025-03-05 20:06:32 +0530

    URLs fetched from api

--- assets/js/api.js
+++ assets/js/api.js
@@ -4,3 +4,43 @@
  * @copyright codewithsadee 2023 All rights reserved
  * @author codewithsadee <mohammadsadee24@gmail.com>
  */
+
+'use strict';
+
+const api_key="5aab401b410423cead0a3da58e8aef06";
+
+/**
+ * 
+ * @param {string} URL API url
+ * @param {Function} callback callback
+ */
+
+
+export const fetchData=function(URL,callback){
+    fetch(`${URL}&appid=${api_key}`)
+    .then(res=>res.json())
+    .then(data=>callback(data))
+}
+
+export const url={
+    currentWeather(lat,lon){
+        return `https://api.openweathermap.org/data/2.5/weather?${lat}&${lon}&units=metric`
+    },
+    forecast(lat,lon){
+        return `https://api.openweathermap.org/data/2.5/forecast?${lat}&${lon}&units=metric`
+    },
+    airPollution(lat,lon){
+        return `http://api.openweathermap.org/data/2.5/air_pollution?${lat}&${lon}`
+    },
+    reverseGeo(lat,lon){
+        return `http://api.openweathermap.org/geo/1.0/reverse?${lat}&${lon}&limit=5`
+    },
+    /**
+     * 
+     * @param {string} query Search query ex-"London", New York
+     * 
+     */
+    geo(query){
+        return `http://api.openweathermap.org/geo/1.0/direct?q=${query}&limit=5`
+    }
+}
